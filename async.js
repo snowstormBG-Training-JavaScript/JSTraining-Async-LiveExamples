00bd182b9b1767da4279b1dc7126bdec62231e1d
snowstormBG <snowstormsu@gmail.com>
2020-11-11 16:00:09 +0200

    Promise.race() ; Promise.all() and Promise.allSettled() observed.

--- async.js
+++ async.js
@@ -34,3 +34,47 @@ async function trackUserHandler() {
     }
     console.log(timerData, posData);
 }
+
+//Promise.race , just like the name suggests:
+// we take an array of promises
+// and we return the data of the first that finishes execution
+
+// Promise.race([
+//     getPosition(),
+//     setTimer(4000)
+// ])
+//     .then(data => {
+//             console.log(data);
+//         }
+//     );
+
+
+
+//Promise.all wants every promise in the array to resolve before moving onto the .then()
+// this means either ALL resolve or at least 1 rejected
+
+// Promise.all([
+//     getPosition(),
+//     setTimer(4000)
+// ])
+//     .then(promiseData => {
+//             console.log('Promise all returned data:');
+//             console.log(promiseData);
+//         }
+//     );
+
+
+
+//Promise.allSettled runs all and returns an array with the return data of the promises. Like a report.
+
+Promise.allSettled([
+    getPosition(),
+    setTimer(4000)
+])
+    .then(promiseData => {
+            console.log('Promise all returned data:');
+            console.log(promiseData);
+        }
+    );
+
+
